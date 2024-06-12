b389c5a9ff3c5105249fea271b504481915d3a2d
an-sche <bscheit92@gmail.com>
2024-06-12 02:43:51 -0400

    Adds admin portal for modifying Categories and Tags

--- Data.Models/Comment.cs
+++ Data.Models/Comment.cs
@@ -1,10 +1,14 @@
-﻿namespace Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models;
 
 public class Comment
 {
     public string? Id { get; set; }
-    public required string BlogPostId { get; set;}
-    public DateTime Date { get; set;}
-    public string Text {get; set;} = string.Empty;
-    public string Name {get; set;} = string.Empty;
+    public required string BlogPostId { get; set; }
+    public DateTime Date { get; set; }
+    [Required]
+    public string Text { get; set; } = string.Empty;
+    [Required]
+    public string Name { get; set; } = string.Empty;
 }
